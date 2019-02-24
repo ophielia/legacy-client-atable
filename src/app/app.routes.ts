a55f416f128f014c5fd4ca0247750eabbe7aa7a8
ophielia <ophielia@yahoo.com>
2019-02-24 22:44:58 +0100

    change: home page updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,15 @@
 import {RouterModule, Routes} from "@angular/router";
 import {TagListComponent} from "./legacy/tag-list/tag-list.component";
 import {AddTagComponent} from "./legacy/tag-list/add-tag.component";
-import {EditTagComponent} from "./legacy/tag-list/edit-tag.component";
-import {HomeComponent} from "./legacy/home.component";
 import {LoginComponent} from "./legacy/login/login.component";
 import {EditDishComponent} from "./legacy/dish-list/edit-dish.component";
 import {DishListComponent} from "./legacy/dish-list/dish-list.component";
 import {MealPlanListComponent} from "./legacy/meal-plan-list/meal-plan-list.component";
-import {EditMealPlanComponent} from "./legacy/meal-plan-list/edit-meal-plan.component";
 import {ShoppingListListComponent} from "./legacy/shopping-list-list/shopping-list-list.component";
-import {EditShoppingListComponent} from "./legacy/shopping-list-list/edit-shopping-list.component";
 import {DishTagAssignToolComponent} from "./legacy/dish-tag-assign-tool/dish-tag-assign-tool.component";
 import {ListLayoutListComponent} from "./legacy/list-layout-list/list-layout-list.component";
-import {EditListLayoutComponent} from "./legacy/list-layout-list/edit-list-layout.component";
-import {ListTagAssignToolComponent} from "./legacy/list-layout-list/list-tag-assign-tool.component";
 import {TagTagAssignToolComponent} from "./legacy/tag-tag-assign-tool/tag-tag-assign-tool.component";
 import {TargetListComponent} from "./legacy/target-list/target-list.component";
-import {TargetEditComponent} from "./legacy/target-list/target-edit.component";
-import {ProposalEditComponent} from "app/legacy/proposal-edit/proposal-edit.component";
 import {RatingTagAssignToolComponent} from "./legacy/rating-tag-assign-tool/rating-tag-assign-tool.component";
 import {LandingPadComponent} from "app/dashboard/landing-pad/landing-pad.component";
 
@@ -36,7 +28,7 @@ const routes: Routes = [
   {path: 'mealplan/list', component: MealPlanListComponent},
   {path: 'tools/dishtotag', component: DishTagAssignToolComponent},
   {path: 'tools/ratingtag', component: RatingTagAssignToolComponent},
-  {path: '', redirectTo: 'dish/list', pathMatch: 'full'}
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 export const routing = RouterModule.forRoot(routes);
